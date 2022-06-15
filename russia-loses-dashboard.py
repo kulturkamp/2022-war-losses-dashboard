c8cba830f9ef34c2a80be4ebbefb805cf6d81ada
kulturkamp <dgtrv123@gmail.com>
2022-06-15 18:51:56 +0300

    added last day metrics and bar/scatter plots for units

--- russia-loses-dashboard.py
+++ russia-loses-dashboard.py
@@ -81,7 +81,7 @@ with st.container():
         attribute_ = st.selectbox(
             label='Select unit', 
             options=cols, 
-            index=6
+            index=1
         )
 
     fig = make_subplots(2, 1, subplot_titles=['Total losses', 'Daily losses'], shared_xaxes=True, vertical_spacing = 0.1)
