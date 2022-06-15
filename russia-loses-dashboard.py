edcfa71d7d736d05557c229328ba4352e0b78efb
kulturkamp <dgtrv123@gmail.com>
2022-06-15 14:15:36 +0300

    added requirements

--- russia-loses-dashboard.py
+++ russia-loses-dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.colors as clrs
